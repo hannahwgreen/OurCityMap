601e0c64420f774cc923392f1a3caeb44067c389
Hannah <hannahwgreen1@gmail.com>
2018-03-29 14:22:14 -0400

    popups working

--- app/javascript/containers/MapContainer.js
+++ app/javascript/containers/MapContainer.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import PhotoFormContainer from './PhotoFormContainer'
 import PhotoStreamContainer from './PhotoStreamContainer';
 import ReactMapboxGl, { Layer, Feature, Popup } from "react-mapbox-gl";
+import { Link } from 'react-router';
 
 const Map = ReactMapboxGl({
   accessToken: "pk.eyJ1IjoiaGFubmFod2dyZWVuIiwiYSI6ImNqZmEyYm94bjI5a2Eyd3BkZjU4em1pZzYifQ.JGiB3u_tzlM4zQM-GJDsgA"
@@ -11,36 +13,74 @@ class MapContainer extends React.Component {
     super(props);
     this.state = {
       selectedPhoto: null,
+      newPhotoCoordinates: [],
+      center: [-75.16416549682616, 39.95054298488249],
         }
+
+        this.handleMarkerClick = this.handleMarkerClick.bind(this);
+        this.onClickMap = this.onClickMap.bind(this);
       }
 
-      // handleMarkerClick(e){
-      //   e.target.value
-      //   this.props.photos.each {
-      //     if (photo.id == e.target.id){
-      //       photo.visibility = true
-      //     } else {
-      //       photo.visibility = false
-      //     }
-      //   }
-      // }
+      handleMarkerClick(e){
+        debugger
+        let center = [e.feature.properties.lng, e.feature.properties.lat]
+        this.props.photos.map( photo => {
+          if (photo.id == e.feature.properties.photoId){
+            this.setState({selectedPhoto: photo, center: center})
+          }
+        })
+      }
+
+      onClickMap(map, evt) {
+        let lng = evt.lngLat.lng
+        let lat = evt.lngLat.lat
+        this.setState({newPhotoCoordinates: [lng, lat]})
+      }
+
+
+
 
       render() {
 
         let markersArr = this.props.photos.map(photo => {
+          let lng = Number(photo.coordinates[0])
+          let lat = Number(photo.coordinates[1])
+          let properties = {photoId: photo.id, lng: lng, lat: lat}
           return(
             <Feature
-              coordinates={photo.coordinates} />
+              key={photo.id}
+              photoId={photo.id}
+              coordinates={photo.coordinates}
+              onClick={this.handleMarkerClick}
+              properties={properties}
+            />
             )
           })
 
-          let popups = this.props.photos.map(photo => {
-            if (photo.visible == true){
+          let newPhotoPopup = this.state.newPhotoCoordinates.map(coordinates => {
+            return(
+              <Popup
+                coordinates={this.state.newPhotoCoordinates}
+                anchor="bottom">
+                  <div><Link to={`/photos/new`}><button type="button" className="btn btn-success float-right">Add New Photo</button></Link></div>
+              </Popup>
+            )
+
+            // return(
+            //   <PhotoFormContainer
+            //     coordinate={this.state.newPhotoCoordinates}
+            //   />
+            // )
+          })
+
+          let photoPopups = this.props.photos.map(photo => {
+            if (photo == this.state.selectedPhoto){
               return(
                 <Popup
+                  key={photo.id}
                   coordinates={photo.coordinates}
                   anchor="top">
-                  <img src={photo.image_url} width='150px' height= '150px'></img>
+                  <img src={photo.image_url} width='180px' height= '180px'></img>
                 </Popup>
               )
             }
@@ -49,15 +89,15 @@ class MapContainer extends React.Component {
           return(
               <div>
                 <Map
-                  center={[-75.16416549682616,
-                    39.95054298488249]}
-                    zoom={[10]}
+                  center={this.state.center}
+                    onClick={this.onClickMap}
                     style="mapbox://styles/mapbox/streets-v9"
                     containerStyle={{
                       height: "500px",
                       width: "800px"
                     }}>
-                    {popups}
+                    {photoPopups}
+                    {newPhotoPopup}
                     <Layer
                       type="symbol"
                       id="marker"
